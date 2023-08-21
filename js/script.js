dcbf573e71b990a1157bd1850f5e2dab89f775e6
Oleg Mechytashvili <63786049+OlegMechitashvili@users.noreply.github.com>
2023-08-21 09:41:20 +0000

    feat: lesson 103

--- js/script.js
+++ js/script.js
@@ -6,4 +6,30 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+    order: function(starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+    }
 };
+
+// const [first, , third] = restaurant.categories;
+// console.log(first, third);
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [firstMeal, secondMeal] = restaurant.order(2, 0);
+console.log(firstMeal, secondMeal);
+
+// Nested destructuring
+const nested = [2, 4, [5, 9]];
+const [a, ,[b, c]] = nested;
+console.log(a, b, c);
+
+// Default velues
+ const [p = 1, q = 1, r = 1] = [8, 9];
+ console.log(p, q, r);
