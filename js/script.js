888ea62f731ca79216125347b29280561494cba2
Oleg Mechytashvili <63786049+OlegMechitashvili@users.noreply.github.com>
2023-08-21 10:02:17 +0000

    feat lesson 104 midle

--- js/script.js
+++ js/script.js
@@ -6,12 +6,46 @@ const restaurant = {
     categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
     starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
     mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    openingHours: {
+            thu: {
+                open: 12,
+                close: 22,
+            },
+            fri: {
+                open: 11,
+                close: 23,
+            },
+            sat: {
+                open: 0,
+                close: 24,
+            },
+    },
 
     order: function(starterIndex, mainIndex) {
         return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     }
 };
 
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+// Renaming destructed variables:
+const{name: restaurantName, openingHours: hours, categories: tags} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Sat a default value
+const {menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+
+
+
+
+
+
+/*
+///////////////////////////////////////////
+DESTRUCTURING ARRAYS
+///////////////////////////////////////////
 // const [first, , third] = restaurant.categories;
 // console.log(first, third);
 
@@ -33,3 +67,4 @@ console.log(a, b, c);
 // Default velues
  const [p = 1, q = 1, r = 1] = [8, 9];
  console.log(p, q, r);
+ */
